b6e97f629dd58710b7d24b57c125bfd4ab004487
poorvakhandare3006 <poorvakhandare1999@gmail.com>
2020-06-23 19:58:58 +0530

    changes

--- faculty/urls.py
+++ faculty/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf.urls import url ,include
+
 from django.urls import path
 from . import views
 
@@ -8,4 +10,6 @@ urlpatterns = [
     path("register/",views.register,name="registerstudent"),
     path("login/",views.login,name="login"),
     path("logout/",views.logout,name="logout"),
+    url(r'^activatemembers/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.activate, name='activate'),
 ]
